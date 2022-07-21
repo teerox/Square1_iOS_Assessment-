8f40962d8047c80083977ccb58a0582485c2fa79
Tony <aodu@getcarbon.co>
2022-07-21 01:00:27 +0100

    Initial Commit

---
+++ Square1_iOS_Assessment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Square1_iOS_Assessment
+//
+//  Created by Anthony Odu on 2022/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Square1_iOS_AssessmentUITests/Square1_iOS_AssessmentUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Square1_iOS_AssessmentUITestsLaunchTests.swift
+//  Square1_iOS_AssessmentUITests
+//
+//  Created by Anthony Odu on 2022/7/21.
+//
+
+import XCTest
+
+class Square1_iOS_AssessmentUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
